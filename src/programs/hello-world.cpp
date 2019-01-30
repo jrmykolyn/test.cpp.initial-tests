811d204229d6a1356839f5664e93f1255f5e8c6e
Jesse R Mykolyn <jesse.mykolyn@groupbyinc.com>
2019-01-30 08:05:56 -0500

    Complete first pass of 'hello world'-type program

---
+++ src/programs/hello-world.cpp
@@ -0,0 +1,8 @@
+# include <iostream>
+
+int main() {
+  std::cout << "Please enter your name:" << " ";
+  std::string name;
+  std::cin >> name;
+  std::cout << "Hello, my name is" << " " << name << "\n";
+}
